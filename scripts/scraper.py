480c74cc5b74c820b85dd9c55ac5404642247eaf
sean mcclure <55333380+smcclure17@users.noreply.github.com>
2023-08-09 12:51:47 -0400

    Rename columns and format dates for Flourish ingestion (#1)
    * rename columns and parse dates
    
    * docs

--- scripts/scraper.py
+++ scripts/scraper.py
@@ -1,16 +1,39 @@
 import requests
 from csv import DictWriter
 from bs4 import BeautifulSoup
+from datetime import datetime
 
 global report_table, final_row
 
-field_names = ['txtEntryDate', 'txtNewlyEligible', 'txtDirect', 'txtPresumptive', 'txtMotelFamilies']
+field_names = {
+  "txtEntryDate": "Date",
+  "txtNewlyEligible": "Newly eligible families",
+  "txtDirect": "Directly placed families",
+  "txtPresumptive": "Presumptively placed families",
+  "txtMotelFamilies": "Families placed in motels"
+}
 
 HEADERS = {
   'user-agent': ('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36'),
   'accept-language': 'he-IL,he;q=0.8,en-US;q=0.6,en;q=0.4',
   'cache-control': 'max-age=0'
 }
+
+def parse_date(date: str):
+  """Parses a string date
+
+    Example: 
+    ```python
+      parse_date("Tuesday, August 8, 2023")  # 8/8/2023
+    ```
+
+  Args:
+      date (str): The date to parse
+  Returns:
+      str: The parsed date
+  """
+  parsed_date = datetime.strptime(date, "%A, %B %d, %Y")
+  return parsed_date.strftime("%-m/%-d/%Y")
 
 request = requests.get('https://hed-dhsentry.azurewebsites.net/default.aspx', HEADERS)
 
@@ -43,10 +66,12 @@ with open('data/housing-stabilization.csv', 'r') as data_object:
   final_row = data_object.readlines()[-1]
   print(final_row)
 
+data["txtEntryDate"] = parse_date(data["txtEntryDate"])  # Parse the date to the desired format
 print(final_row[0], data['txtEntryDate'])
 if data['txtEntryDate'] not in final_row:
+  data = {field_names[key]: value for (key, value) in data.items()}  # Rename the keys to human-readable format
   with open('data/housing-stabilization.csv', 'a') as data_object:
-    dictwriter_object = DictWriter(data_object, fieldnames=field_names)
+    dictwriter_object = DictWriter(data_object, fieldnames=field_names.values())
     dictwriter_object.writerow(data)
   
 
